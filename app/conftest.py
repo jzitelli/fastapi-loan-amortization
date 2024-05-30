7fc94a8025877daebec0476dd5df98fa12bbe6a5
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2024-05-30 17:11:32 -0400

    dragging in some pieces (starting with user creation with sqlmodel) from https://github.com/tiangolo/full-stack-fastapi-template

---
+++ app/conftest.py
@@ -0,0 +1,17 @@
+from collections.abc import Generator
+import pytest
+from sqlmodel import Session, delete
+from app.db import engine, init_db
+from app.models import User
+
+
+@pytest.fixture(scope="session", autouse=True)
+def db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        init_db(session)
+        yield session
+        # statement = delete(Item)
+        # session.execute(statement)
+        statement = delete(User)
+        session.execute(statement)
+        session.commit()
---
+++ app/models.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, SQLModel
+
+
+# Shared properties
+# TODO replace email str with EmailStr when sqlmodel supports it
+class UserBase(SQLModel):
+    email: str = Field(unique=True, index=True)
+    is_active: bool = True
+    is_superuser: bool = False
+    full_name: str | None = None
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    password: str
+
+
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
