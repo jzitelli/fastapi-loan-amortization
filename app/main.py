a8b1508489a3e2a07ac12998348d1af0e42140c5
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2024-06-02 09:00:38 -0400

    rm initial stub routes

--- app/main.py
+++ app/main.py
@@ -10,16 +10,6 @@ app = FastAPI()
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
-@app.get("/loans/{loan_id}/schedule")
-def fetch_loan_schedule(loan_id: str):
-    pass
-
-
-@app.get("/loans/{loan_id}/summary")
-def fetch_loan_summary(loan_id: str, month: int):
-    pass
-
-
 @app.get("/users/{user_id}/loans")
 def fetch_loans_for_user(user_id: str):
     pass
